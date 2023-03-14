41933190399a655a37ab20959810993710d98308
ZildjianVito <zildjianvito07@gmail.com>
2023-03-14 19:05:48 +0700

    done navbar

---
+++ src/container/Testimonials/Testimonial.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Testimonial.scss"
+
+const Testimonial = () => {
+  return (
+    <div>Testimonials</div>
+  )
+}
+
+export default Testimonial
---
+++ src/container/index.js
@@ -0,0 +1,8 @@
+import About from "./About/About";
+import Skills from "./Skills/Skills";
+import Testimonial from "./Testimonials/Testimonial";
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Work from "./Work/Work";
+
+export { About, Skills, Testimonial, Header, Footer, Work };
