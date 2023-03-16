016cacf3acb1b1cb8425633aff08bee95b6a43c7
ZildjianVito <zildjianvito07@gmail.com>
2023-03-16 18:48:11 +0700

    change web app to website

--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -43,7 +43,7 @@ const Work = () => {
       </h2>
 
       <div className="app__work-filter">
-        {["Web App", "React JS", "All"].map((item, index) => (
+        {["Website", "React JS", "All"].map((item, index) => (
           <div
             key={index}
             onClick={() => handleWorkFilter(item)}
